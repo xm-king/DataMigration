0ac93e71dfa3c04d41c816b856709c45b57b4093
xm-king <tianming.xm@gmail.com>
2015-01-14 15:49:37 +0800

    DAO layer

--- src/main/java/com/diantu/dao/MigrationJobMapper.java
+++ src/main/java/com/diantu/dao/MigrationJobMapper.java
@@ -13,6 +13,8 @@ public interface MigrationJobMapper {
 	public List<MigrationJobModel> listJobs();
 	//保存定时任务到数据库
 	public void saveJob(MigrationJobModel jobModel);
+	//根据ID查询出定时任务对象
+	public MigrationJobModel getJob(int id);
 	//更新定时任务到数据库
 	public void updateJob(MigrationJobModel jobModel);
 	
