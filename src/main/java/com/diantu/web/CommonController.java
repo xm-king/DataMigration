e274be799ead2a9eb7ec638edab8a2f5ef269946
xm-king <tianming.xm@gmail.com>
2015-01-15 17:06:52 +0800

    web

--- src/main/java/com/diantu/web/CommonController.java
+++ src/main/java/com/diantu/web/CommonController.java
@@ -2,13 +2,19 @@ package com.diantu.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CommonController {
 	
-	@RequestMapping(value="/login")
-	public void login(@RequestParam("name")String name,@RequestParam("password")String password){
-		
+	@RequestMapping(value="/v1/login",method=RequestMethod.POST)
+	public String login(@RequestParam("name")String name,@RequestParam("password")String password){
+		//if login success redirect index.html
+		if("admin".equals(name) && "admin".equals(password)){
+			return "redirect:/index.html";
+		}else{
+			return "redirect:/login.html"; 
+		}
 	}
 }
